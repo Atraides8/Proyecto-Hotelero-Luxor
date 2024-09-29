d348e81b992d076107b413d319c451e129e68f15
bel-verdugo <134398435+bel-verdugo@users.noreply.github.com>
2024-09-29 20:14:59 -0300

    Update ContactPage.vue
    cambios del color contacto

--- src/views/ContactPage.vue
+++ src/views/ContactPage.vue
@@ -1,19 +1,58 @@
 <template>
-    <div>
-      <Navbar />
-      <ion-content>
-        <!-- Contenido de la página de inicio -->
-      </ion-content>
-    </div>
-  </template>
-  
-  <script>
-  import Navbar from '@/components/Navbar.vue';
-  
-  export default {
-    components: {
-      Navbar
-    }
+  <div class="contact-page">
+    <Navbar />
+    <ion-content>
+      <div class="contact-container">
+        <h1>Contacto</h1>
+        <form>
+          <input type="text" placeholder="Escribe tus datos personales" />
+          <textarea placeholder="Escribe reclamo/sugerencia/consulta"></textarea>
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </ion-content>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
   }
-  </script>
-  
+}
+</script>
+
+<style scoped>
+.contact-page {
+  background-color: black;
+  color: white;
+  height: 100vh;
+  text-align: center;
+}
+
+.contact-container {
+  margin: 0 auto;
+  padding: 20px;
+  max-width: 600px;
+}
+
+input, textarea {
+  display: block;
+  width: 100%;
+  margin-bottom: 20px;
+  padding: 10px;
+  background-color: #b59142;
+  color: white;
+  border: none;
+}
+
+button {
+  background-color: #b59142;
+  color: white;
+  padding: 10px 20px;
+  border: none;
+  cursor: pointer;
+}
+</style>
